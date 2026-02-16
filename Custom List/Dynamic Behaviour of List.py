db026cf031e64554eee3c23686eef61a411d29d8
Shaik Zaheer <zaheershaik2925@gmail.com>
2026-02-16 22:36:57 +0530

    Add files via upload

---
+++ Custom List/Dynamic Behaviour of List.py
@@ -0,0 +1,26 @@
+# import sys 
+
+# l1 = []
+# # print("Initial Size: ", sys.getsizeof(l1))
+# # for i in range(0, 17):
+# #     l1.append(i)
+# #     print(f"{i} --> {sys.getsizeof(l1)}")
+
+
+# a = 1
+# l1.append(1)
+# print(id(1))
+# a = 2
+# print(id(1))
+# print(id(l1[0]))
+# l1.append("Python")
+# print(l1)
+
+
+s = "Python"
+(s[:-1])
+
+l1 = []
+
+print(l1.pop())
+print(l1)
